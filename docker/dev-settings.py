61eed940b63fecedf6b94c441699c72bc0c00c23
Justin Caratzas <bigjust@lambdaphil.es>
2017-11-04 13:36:39 -0400

    move dev-settings.py to docker/

---
+++ docker/dev-settings.py
@@ -0,0 +1,17 @@
+NICK = 'helga'
+SERVER = {
+    'HOST': 'irc',
+    'PORT': 6667,
+    'SSL': False,
+    'USERNAME': 'helga',
+}
+
+CHANNELS = [
+    ('#test',),
+]
+
+DATABASE = {
+    'HOST': 'mongo',
+    'PORT': 27017,
+    'DB': 'helga',
+}
