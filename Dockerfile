7b6cc4233d24af2f0fc9de9cf2551214607464c2
Justin Caratzas <justin.caratzas@gmail.com>
2026-04-25 16:55:41 -0400

    switch to docker compose
    # Conflicts:
    #	Dockerfile
    #	docker-compose.yml
    #	settings-docker.py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
-FROM python:2
+FROM python:3
 
-ADD . /helga
-WORKDIR /helga
+ADD . /code
+WORKDIR /code
 
-RUN pip install . tox
-RUN tox
-RUN pip list -o
-
-ENTRYPOINT ["/usr/local/bin/helga"]
-CMD ["--settings=settings-docker.py"]
+RUN pip install .
--- settings-docker.py
+++ settings-docker.py
@@ -4,18 +4,20 @@ import os
 NICK = os.environ.get('HELGA_NICK', 'helga')
 
 SERVER = {
-    'TYPE': 'irc',
     'HOST': os.environ.get('HELGA_IRC_SERVER', 'localhost'),
     'PORT': 6667,
     'SSL': False,
+    'USERNAME': os.environ.get('HELGA_IRC_USER', 'helga')
 }
 
 CHANNELS = [
-    ('#helga-dev',),
+    ('#test',),
 ]
 
 DATABASE = {
-    'HOST': os.environ.get('HELGA_MONGO_HOST', 'mongo'),
+    'HOST': os.environ.get('HELGA_MONGO_HOST', 'localhost'),
     'PORT': 27017,
-    'DB': os.environ.get('HELGA_MONGO_DB', 'helga'),
+    'DB': os.environ.get('HELGA_MONGO_DB', 'helga')
 }
+
+# Made with Bob
